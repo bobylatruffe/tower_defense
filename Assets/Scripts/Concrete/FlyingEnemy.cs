251350c5f69945b37c579c31e4eec6dc5092aef3
bobylatruffe <bozlak.fatih@gmail.com>
2025-02-15 19:32:56 +0100

    feat(SimpleAEnemieFactory): test de la surchage des mouvements des enemies

--- Assets/Scripts/Concrete/FlyingEnemy.cs
+++ Assets/Scripts/Concrete/FlyingEnemy.cs
@@ -12,7 +12,9 @@ public class FlyingEnemy : A_Enemie
 
     private void Update()
     {
-        move();
+
+        transform.Translate(Vector3.forward * Random.Range(4f, 7f) * Time.deltaTime);
+
 
         if (heightOfFlying > 0)
         {
--- Assets/Scripts/Concrete/WalkingEnemy.cs
+++ Assets/Scripts/Concrete/WalkingEnemy.cs
@@ -6,4 +6,9 @@ public class WalkingEnemy : A_Enemie
     {
         move();
     }
+
+    protected override void move()
+    {
+        transform.Translate(Vector3.forward * Random.Range(1f, 3f) * Time.deltaTime);
+    }
 }
